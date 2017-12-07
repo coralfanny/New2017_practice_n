4c55f517b8bad20a88a9b1f3a92e3648f8788273
mrfangyang <mr.fangyang@gmail.com>
2017-12-07 18:47:57 +0800

    this is just a first git commit test.

---
+++ Downloads/androidDevBasics-master/Ch04/04_03/start/New2017_practice/app/src/test/java/com/timebox/new2017_practice/MainActivityTest.java
@@ -0,0 +1,18 @@
+package com.timebox.new2017_practice;
+
+import org.junit.Test;
+
+/**
+ * Created by shanshan on 2017/11/25.
+ */
+public class MainActivityTest {
+    @Test
+    public void onClick() throws Exception {
+    }
+
+//    @Test
+//    public void emailValidator_CorrectEmailSimple_ReturnsTrue() {
+//        assertThat(EmailValidator.isValidEmail("name@email.com"), is(true));
+//    }
+
+}
